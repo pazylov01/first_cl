41afe87351a4403137704b48bde39205098a2a94
Ваше Имя <you@example.com>
2021-05-04 19:32:13 +0600

    first class

---
+++ main.py
@@ -0,0 +1,18 @@
+class Human:
+    name = 'Petr'
+    job = 'Programmer'
+    born = 'Kyrgyzstan'
+    merried = True
+
+    def start(self):
+        print('Happy birhday')
+
+    def finish(self):
+        print('Good job bro')
+
+
+human_a = Human()
+
+human_b = Human()
+
+print(human_a.name)
